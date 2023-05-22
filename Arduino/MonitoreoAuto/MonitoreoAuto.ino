946f5c3efab54328f36b1e0b02f56ead22f39994
WillFajardo <diasfavian69@gmail.com>
2023-05-22 09:40:28 -0500

    Se movio los documentos a la carperta DOCUMENTOS PROCESO

--- Arduino/MonitoreoAuto/MonitoreoAuto.ino
+++ Arduino/MonitoreoAuto/MonitoreoAuto.ino
@@ -1,12 +1,31 @@
 #include <WiFi.h>
 #include <TinyGPSPlus.h>
+#include <HTTPClient.h>
 
 
 const char* ssid     = "PuntoAcces";      // SSID
 const char* password = "123456789";      // Password
-const char* host = "192.168.100.18";  // Dirección IP local o remota, del Servidor Web
+const char* host = "https://invernaderom.000webhostapp.com/";  // Dirección IP local o remota, del Servidor Web
 const int   port = 80;            // Puerto, HTTP es 80 por defecto, cambiar si es necesario.
 const int   watchdog = 2000;        // Frecuencia del Watchdog
+
+//Variables used in the code
+String LED_id = "1";                  //Just in case you control more than 1 LED
+bool toggle_pressed = false;          //Each time we press the push button    
+String data_to_send = "";             //Text data to send to the server
+unsigned int Actual_Millis, Previous_Millis;
+int refresh_time = 200;               //Refresh rate of connection to website (recommended more than 1s)
+
+
+//Inputs/outputs
+int button1 = 13;                     //Connect push button on this pin
+int LED = 2;                          //Connect LED on this pin (add 150ohm resistor)
+
+
+//Button press interruption
+void IRAM_ATTR isr() {
+  toggle_pressed = true; 
+}
 
 //Variables sensor
 float mVperAmp = 0.100; // Sensibilidad del sensor ACS712 de 20A
@@ -33,7 +52,11 @@ int idDis=2;
 TinyGPSPlus gps;
  
 void setup() {
-
+  
+  delay(10);                  //Start monitor
+  pinMode(LED, OUTPUT);                   //Set pin 2 as OUTPUT
+  pinMode(button1, INPUT_PULLDOWN);       //Set pin 13 as INPUT with pulldown
+  attachInterrupt(button1, isr, RISING);  //Create interruption on pin 13
   pinMode(gpio5_pin, OUTPUT);
   pinMode(gpio18_pin, OUTPUT);
   pinMode(analogInPin, INPUT);
@@ -54,14 +77,70 @@ void setup() {
   Serial.println("WiFi conectado");  
   Serial.println("Dirección IP: ");
   Serial.println(WiFi.localIP());
+//===============================================================================
+
+  Actual_Millis = millis();               //Save time for refresh loop
+  Previous_Millis = Actual_Millis; 
 }
 
 void loop() {
- 
+  //We make the refresh loop using millis() so we don't have to sue delay();
+  Actual_Millis = millis();
   unsigned long currentMillis = millis();
+  
+   if(Actual_Millis - Previous_Millis > refresh_time){
+    Previous_Millis = Actual_Millis;  
+    if(WiFi.status()== WL_CONNECTED){                   //Check WiFi connection status  
+      HTTPClient http;                                  //Create new client
+      if(toggle_pressed){                               //If button was pressed we send text: "toggle_LED"
+        data_to_send = "toggle_LED=" + LED_id;  
+        toggle_pressed = false;                         //Also equal this variable back to false 
+      }
+      else{
+        data_to_send = "check_LED_status=" + LED_id;    //If button wasn't pressed we send text: "check_LED_status"
+      }
+      
+      //Begin new connection to website       
+      http.begin("https://invernaderom.000webhostapp.com/programasSERVIDOR_php/interfaces/esp32_update.php");   //Indicate the destination webpage 
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");         //Prepare the header
+      
+      int response_code = http.POST(data_to_send);                                //Send the POST. This will giveg us a response code
+      
+      //If the code is higher than 0, it means we received a response
+      if(response_code > 0){
+        Serial.println("HTTP code " + String(response_code));                     //Print return code
+  
+        if(response_code == 200){                                                 //If code is 200, we received a good response and we can read the echo data
+          String response_body = http.getString();                                //Save the data comming from the website
+          Serial.print("Server reply: ");                                         //Print data to the monitor for debug
+          Serial.println(response_body);
+
+          //If the received data is LED_is_off, we set LOW the LED pin
+          if(response_body == "LED_is_off"){
+            digitalWrite(LED, HIGH);
+          }
+          //If the received data is LED_is_on, we set HIGH the LED pin
+          else if(response_body == "LED_is_on"){
+            digitalWrite(LED, HIGH);
+            delay(500);
+            digitalWrite(LED, LOW);
+          }  
+        }//End of response_code = 200
+      }//END of response_code > 0
+      
+      else{
+       Serial.print("Error sending POST, code: ");
+       Serial.println(response_code);
+      }
+      http.end();                                                                 //End the connection
+    }//END of WIFI connected
+    else{
+      Serial.println("WIFI connection error");
+    }
+  }
 
   //CONSULTA DE ESTADO DE DISPOSITIVO 
-  String url = "/programas_php/proceso_eventos/estadoDis.php?idDis=";
+  String url = "/programasSERVIDOR_php/proceso_eventos/estadoDis.php?idDis=";
   url += idDis;
 
 
@@ -122,7 +201,7 @@ void loop() {
     //t = 1;
     //Serial.print("Dispositivo activo = ");
     
-    String url = "/programas_php/proceso_eventos/programa1.php?estado_vehiculo=";
+    String url = "/programasSERVIDOR_php/proceso_eventos/programa1.php?estado_vehiculo=";
     url += h;
     url += "&estado_dispositivo=";
     url += t;
