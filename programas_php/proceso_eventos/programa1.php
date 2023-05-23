2513772b3dbda050626821ba6296c2fbd4453568
WilliamFajardo <wifadiaz@unicauca.edu.co>
2023-05-23 16:47:36 -0500

    Se ha modificado las lineas de codigo de values y se ha quitado el condicional if

--- programas_php/proceso_eventos/programa1.php
+++ programas_php/proceso_eventos/programa1.php
@@ -11,17 +11,13 @@ $idDis = $_GET["idDis"];
 
 $mysqli = new mysqli($host, $user, $pw, $db); // Aqu� se hace la conexi�n a la base de datos.
 
-//date_default_timezone_set('America/Bogota'); // esta l�nea es importante cuando el servidor es REMOTO y est� ubicado en otros pa�ses como USA o Europa. Fija la hora de Colombia para que grabe correctamente el dato de fecha y hora con CURDATE y CURTIME, en la base de datos.
+date_default_timezone_set('America/Bogota'); // esta l�nea es importante cuando el servidor es REMOTO y est� ubicado en otros pa�ses como USA o Europa. Fija la hora de Colombia para que grabe correctamente el dato de fecha y hora con CURDATE y CURTIME, en la base de datos.
 
-//$fecha = date("Y-m-d");
-//$hora = date("h:i:s");
+$fecha = date("Y-m-d");
+$hora = date("h:i:s");
 
-$sql1 = "INSERT into datos_medidos (idVeh,estado_vehiculo,idDis,estado_dispositivo,latitud,longitud, fecha, hora) VALUES ((SELECT idVeh FROM datos_vehiculo WHERE idVeh = '$idVeh'),'$estado_vehiculo',(SELECT idDis FROM datos_dispositivo WHERE idDis = '$idDis'),'$estado_dispositivo','$latitud','$longitud', CURDATE(), CURTIME())"; // Aqu� se ingresa el valor recibido a la base de datos.
+$sql1 = "INSERT into datos_medidos (idVeh,estado_vehiculo,idDis,estado_dispositivo,latitud,longitud, fecha, hora) VALUES ('$idVeh','$estado_vehiculo', '$idDis' ,'$estado_dispositivo','$latitud','$longitud', '$fecha', '$hora')"; // Aqu� se ingresa el valor recibido a la base de datos.
 echo "sql1...".$sql1; // Se imprime la cadena sql enviada a la base de datos, se utiliza para depurar el programa php, en caso de alg�n error.
 $result1 = $mysqli->query($sql1);
 echo "result es...".$result1; // Si result es 1, quiere decir que el ingreso a la base de datos fue correcto.
-	if ($result1 == 1){
-	$sql1="INSERT into datos_vehiculo (latitud) VALUES ((SELECT latitud FROM datos_medidos WHERE idVeh = '$idVeh'))"	;
-	$result1 = $mysqli->query($sql1);
-}
 ?>
