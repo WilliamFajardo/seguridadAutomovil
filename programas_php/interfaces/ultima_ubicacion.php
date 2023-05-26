97bbd6a6bf8de0cf2410494061b5167ee12f1a42
WilliamFajardo <wifadiaz@unicauca.edu.co>
2023-05-25 21:23:42 -0500

    Archivo que muestra la ultima ubicacion con key de google maps: Aun se puede mejorar la parte visual

--- programas_php/interfaces/ultima_ubicacion.php
+++ programas_php/interfaces/ultima_ubicacion.php
@@ -1,59 +1,362 @@
 <?php
-include "conexion.php";  // Conexión tiene la información sobre la conexión de la base de datos.
-$mysqli = new mysqli($host, $user, $pw, $db); // Aquí se hace la conexión a la base de datos.
+
+// PROGRAMA DE MENU CONSULTA
+include "conexion.php";
+
+session_start();
+if ($_SESSION["autenticado"] != "SIx3")
+      {
+      header('Location: index.php?mensaje=3');
+      }
+else
+      {      
+            $mysqli = new mysqli($host, $user, $pw, $db);
+    	  $sqlusu = "SELECT * from tipo_usuario where id='2'"; //CONSULTA EL TIPO DE USUARIO CON ID=2, TIPO CONSULTA
+        $resultusu = $mysqli->query($sqlusu);
+        $rowusu = $resultusu->fetch_array(MYSQLI_NUM);
+  	    $desc_tipo_usuario = $rowusu[1];
+        if ($_SESSION["tipo_usuario"] != $desc_tipo_usuario)
+          header('Location: index.php?mensaje=4');
+      }
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-         <title> Ultima ubicacion de su vehiculo:</title>
-        </head>
-       <body>
-        <table width="100%" align=center cellpadding=5 border=0 bgcolor="#FFFFFF">
-    	   <tr>
-           <td valign="top" align=left width=70%>
-              <table width="100%" align=center border=0>
-            	   <tr>
-                  <td valign="top" align=center width=30%>
-                     <img src="img/monitoreo.png" border=0 width=350 height=80> 
-             	    </td>
-            <td valign="top" align=center width=60%>
-            <h1><font color=black>Sistema de Seguridad Vehicular </font></h1>
-        </td>
-        <td valign="top" align=right>
-            <font FACE="arial" SIZE=2 color="#000000"> <b><u><?php  echo "Nombre Usuario</u>:".$_SESSION["nombre_usuario"];?> </b></font><br>
-            <font FACE="arial" SIZE=2 color="#000000"> <b><u><?php  echo "Tipo Usuario</u>:   ".$desc_tipo_usuario;?> </b></font><br>  
-             <font FACE="arial" SIZE=2 color="#00FFFF"> <b><u> <a href="inicio_sesion.php"> Cerrar Sesion </a></u></b></font>  
-        </td>
-           	    </tr>
-         	    </table>
-           </td>
-	     </tr>
-     </table>
-         <table width="100%" align=center cellpadding=5 border=0 bgcolor="#FFFFFF">
-                          <a href="https://invernaderom.000webhostapp.com/programasSERVIDOR_php/interfaces/inicial_propietario.php">
-            <button>Volver</button>
-            </a>
-                <?php
-                        include "menu_propietario.php";
-                        ?>
-	  	    <tr valign="top">
-             <td height="20%" align="left" 				
-                    bgcolor="#FFFFFF" class="_espacio_celdas" 					
-                    style="color: #FFFFFF; 
-			             font-weight: bold">
-                 <td valign="top" align=center width=100%>
-			    <font FACE="arial" SIZE=2 color="#1a5276" align=left> <b><h1>Última ubicación  </h1></b></font>  
+    <head>
+     <title>Ultima ubicacion</title>
+     <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+     <link rel="stylesheet" type="text/css" href="estilo.css">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet"> 
 
-		       </td>
-		     </tr>
-		    </table>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
-	
-</head>
-<body>
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
     
+    
+  <style>
+    body {
+      font-family: 'Roboto', Arial, sans-serif;
+      background-color: #FFFFFF;
+      margin: 0;
+      padding: 0;
+    }
+    
+    #map-container {
+        width: 100%;
+        height: 400px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin: 20px auto;
+    }
+    #header {
+      background-color: #1F618D;
+      text-align: center;
+      padding: 20px;
+    }
+    #mapa {
+     width: 100%;
+     height: 400px;
+     border-radius: 5px;
+     margin: 20px auto;
+    }
+    
+    #header img {
+      max-width: 100%;
+      height: auto;
+    }
+    
+    #content {
+      background-color: #D4E6F1;
+      display: flex;
+      justify-content: space-between;
+      margin: 20px;
+      padding: 20px;
+      border-radius: 10px;
+    }
+    
+    #sidebar {
+      width: 30%;
+      background-color: #1F618D;
+      color: white;
+      padding: 20px;
+      border-radius: 10px;
+    }
+    
+   
+    
+    #sidebar form {
+      margin-bottom: 20px;
+    }
+    
+    #sidebar label {
+      display: block;
+      color: white;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+    
+    #sidebar input[type="text"],
+    #sidebar input[type="password"] {
+      width: 100%;
+      padding: 8px;
+      margin-bottom: 10px;
+      border-radius: 5px;
+      border: none;
+    }
+    
+    #sidebar input[type="submit"] {
+      background-color: #2980B9;
+      color: white;
+      padding: 8px 20px;
+      border: none;
+      cursor: pointer;
+      border-radius: 5px;
+      font-weight: bold;
+    }
+    
+    #sidebar input[type="submit"]:hover {
+      background-color: #1F618D;
+    }
+    
+    #error-message {
+      background-color: #FFDDDD;
+      color: #FF0000;
+      font-weight: bold;
+      padding: 10px;
+      margin-bottom: 20px;
+      border-radius: 5px;
+    }
+    
+    #content-container {
+      width: 65%;
+      border-radius: 10px;
+      background-color: #FFFFFF;
+      padding: 20px;
+    }
+    
+    #content h2 {
+      font-size: 24px;
+      margin-top: 0;
+      margin-bottom: 10px;
+      font-weight: bold;
+    }
+    
+    #content ul {
+      margin-bottom: 10px;
+      padding-left: 20px;
+    }
+    
+    #content ul li {
+      margin-bottom: 5px;
+    }
+    
+    .logo-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 20px;
+    }
+    
+    .logo-container img {
+      max-width: 100%;
+      height: auto;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+    
+    /* Encabezado */
+    #header {
+      background-color: #1F618D;
+      padding: 20px;
+      color: white;
+    }
+    
+    #header h1 {
+      text-align: center;
+      margin: 0;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+    
+    /* Menú de navegación */
+    #navigation {
+      background-color: #2980B9;
+      padding: 10px;
+    }
+    
+    #navigation ul {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      display: flex;
+    }
+    
+    #navigation li {
+      margin-right: 20px;
+    }
+    
+    #navigation a {
+      text-decoration: none;
+      color: white;
+      font-weight: bold;
+      font-size: 16px;
+    }
+    
+    /* Contenido principal */
+    #content {
+      padding: 20px;
+    }
+    
+    #content h2 {
+      font-size: 24px;
+      margin: 0 0 10px 0;
+      font-weight: bold;
+    }
+    
+    #content p {
+      margin-bottom: 10px;
+    }
+    
+    /* Pie de página */
+    #footer {
+      background-color: #1F618D;
+      padding: 20px;
+      color: white;
+      text-align: center;
+    }
+    h1 {
+      text-align: center;
+      margin: 0;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+    .card {
+        border: 1px solid #1A5276;
+        background-color: #F2F2F2;
+        padding: 20px;
+        margin-bottom: 20px;
+        text-align: center;
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+//ESTILOS PARA PRESENTACION DE LA INFORMACION
+     .card {
+        border: 1px solid #2980B9;
+        background-color: #F8F9F9;
+        padding: 20px;
+        margin-bottom: 20px;
+        text-align: center;
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
 
+    .card-header {
+        background-color: #2980B9;
+        color: #FFFFFF;
+        font-weight: bold;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    }
+
+    .card-content {
+        padding: 10px;
+        color: #333333;
+        line-height: 1.4;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 30px;
+    }
+
+  </style>
+    </head>
+    <body>
+         <!-- Topbar Start -->
+     <div class="container-fluid">
+        <div class="row bg-secondary py-2 px-lg-5">
+            
+        </div>
+        <div class="row py-3 px-lg-5">
+            <div class="col-lg-4">
+                <a href="" class="navbar-brand d-none d-lg-block">
+                    <img class="logo" src="logo.jpeg" alt="Logo de la empresa" style="width: 200px; height: auto;">
+                </a>
+            </div>
+            <div class="col-lg-8 text-center text-lg-right">
+                <div class="d-inline-flex align-items-center">
+                    <div class="d-inline-flex flex-column text-center pr-3 border-right">
+                        <h6>Horas de atencion</h6>
+                        <p class="m-0">8.00AM - 9.00PM</p>
+                    </div>
+                    <div class="d-inline-flex flex-column text-center px-3 border-right">
+                        <h6>Correo electronico</h6>
+                        <p class="m-0">info@monitoreoSatelital.com</p>
+                    </div>
+                    <div class="d-inline-flex flex-column text-center pl-3">
+                        <h6>Llamanos</h6>
+                        <p class="m-0">+57 3105192099</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+     <!-- Topbar End -->
+     
+     <div id="navigation">
+    <ul>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+    </ul>
+  </div>
+     <table width="100%" align=center cellpadding=5 border=0 bgcolor="#FFFFFF">
+        <tr>
+            <td valign="top" align=right>
+                <font FACE="arial" SIZE=2 color="#000000"> <b><u><?php echo "Nombre Usuario</u>:   " . $_SESSION["nombre_usuario"]; ?></b></font><br>
+                <font FACE="arial" SIZE=2 color="#000000"> <b><u><?php echo "Tipo Usuario</u>:   " . $desc_tipo_usuario; ?></b></font><br>
+                <font FACE="arial" SIZE=2 color="#00FFFF"> <b><u><a href="cerrar_sesion.php"> Cerrar Sesion</a></u></b></font>
+            </td>
+        </tr>
+     </table>
+        <table width="100%" align=center cellpadding=5 border=0 bgcolor="#FFFFFF">
+        <?php
+        include "menu_propietario.php";
+        ?>
+        <td height="20%" align="left" 				
+            bgcolor="#FFFFFF" class="_espacio_celdas" 					
+            style="color: #FFFFFF; 
+            font-weight: bold">
+            <font FACE="arial" SIZE=2 color="#000044"> <b><h1>Última ubicación </h1></b></font>  
+        </tr>
+		</table>
+    </head>
 
 <!DOCTYPE html>
 <html>
@@ -69,15 +372,23 @@ $mysqli = new mysqli($host, $user, $pw, $db); // Aquí se hace la conexión a la
 <?php
 include "conexion.php";  // Conexión tiene la información sobre la conexión de la base de datos.
 $mysqli = new mysqli($host, $user, $pw, $db); // Aquí se hace la conexión a la base de datos.
-$sqlubi = "SELECT * from datos_medidos where idVeh=2 order by id DESC LIMIT 1"; //CONSULTA LA ULTIMA UBICACION AGREGADA A LA TABLA UBICACIONES
+$nombre_usuario=$_SESSION["nombre_usuario"];
+$sqlubi1 = "SELECT * from usuarios where nombre_completo= '$nombre_usuario'"; //CONSULTA LA ULTIMA UBICACION AGREGADA A LA TABLA UBICACIONES
+$result1 = $mysqli->query($sqlubi1);
+$row1 = $result1->fetch_array(MYSQLI_NUM);
+$nombre_completo= $row1[1];
+$id_veh=$row1[8];
+
+
+if($nombre_completo==$nombre_usuario){
+$sqlubi = "SELECT * from datos_medidos where idVeh='$id_veh' order by id DESC LIMIT 1"; //CONSULTA LA ULTIMA UBICACION AGREGADA A LA TABLA UBICACIONES
 $resultubi = $mysqli->query($sqlubi);
 $rowubi = $resultubi->fetch_array(MYSQLI_NUM);
 $latitud = $rowubi[5];
 $longitud = $rowubi[6];
-
+}
 ?>
-
-    
+ 
     <div id="map"></div>
     <script>
       function initMap() {
@@ -88,19 +399,21 @@ $longitud = $rowubi[6];
        
         
         
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 20,
-          center: uluru
-          
+        
+        
+        var map = new google.maps.Map(document.getElementById('map'),{
+          zoom: 10,
+          center:  uluru
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position:  uluru,
           map: map
         });
       }
     </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"> <!-- Se deben reemplazar el espacio vacio por la API Key de Google MAPS, si se quiere ver el mapa sin el mensaje de "For development purposes only"-->
-    </script>
+    
+       <div id="mapa"><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=initMap"></script></h1></div>
+    
+    
   </body>
 </html>
